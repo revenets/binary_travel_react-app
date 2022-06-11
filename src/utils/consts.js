024806a63a09bedc3cf5caab46379a58ee46a334
Revenets <revenets@gmail.com>
2022-06-11 17:34:29 +0300

    Reform static url paths

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,5 @@
 export const SIGN_IN_ROUTE = 'sign-in'
 export const SIGN_UP_ROUTE = 'sign-up'
 export const MAIN_ROUTE = '/'
-export const TRIP_ROUTE = 'trip'
+export const TRIP_ROUTE = 'trip/'
 export const BOOKINGS_ROUTE = 'bookings'
