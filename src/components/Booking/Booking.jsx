7f38a7a9ed59c4bd3de67f5881efcf91b2f1aca0
Revenets <revenets@gmail.com>
2022-06-12 11:02:51 +0300

    Complete bookings rendering logic

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { formatDate } from "../../utils/assists";
 
-const Booking = () => {
+const Booking = ({guests, date, trip, totalPrice: price, cancelBooking}) => {
+
+
     return (
         <li className="booking">
-            <h3 className="booking__title">Iceland</h3>
-            <span className="booking__guests">2 guests</span>
-            <span className="booking__date">13.07.2022</span>
-            <span className="booking__total">14000 $</span>
-            <button className="booking__cancel" title="Cancel booking">
+            <h3 className="booking__title">{trip.title}</h3>
+            <span className="booking__guests">{guests} guests</span>
+            <span className="booking__date">{formatDate(date)}</span>
+            <span className="booking__total">{price} $</span>
+            <button className="booking__cancel" title="Cancel booking" onClick={cancelBooking}>
                 <span className="visually-hidden">Cancel booking</span>×
             </button>
         </li>
--- src/components/Booking/Bookings.jsx
+++ src/components/Booking/Bookings.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import Booking from "./Booking";
+import { bookings } from '../../data/bookings'
 
 const Bookings = () => {
+    const [allBookings, setAllBookings] = useState([...bookings]);
+
+    const renderBookings = allBookings.map(booking => {
+        return <Booking key={booking.id} {...booking} cancelBooking={() => cancelBooking(booking.id)} />
+    })
+
+    function cancelBooking (id) {
+        setAllBookings(prevState => {
+            const newState = prevState.filter(booking => booking.id !== id);
+            return newState
+        })
+    }
+
     return (
         <main className="bookings-page">
             <h1 className="visually-hidden">Travel App</h1>
             <ul className="bookings__list">
-                <Booking />
-                <Booking />
-                <Booking />
+                {renderBookings}
             </ul>
         </main>
     );
